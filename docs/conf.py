b12bcb70f0089953ab849135829e8eb5779bbb9a
desty2k <wojciech.wentland@int.pl>
2021-06-14 08:32:42 +0200

    FIX: ignore import not at the top of file

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('..'))
 
-import QtPyNetwork
+import QtPyNetwork  # noqa
 
 # -- Project information -----------------------------------------------------
 
