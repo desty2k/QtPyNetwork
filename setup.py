e8c2f6bd1c96155e2cbb5e0110964a44143523ab
desty2k <wojciech.wentland@int.pl>
2021-06-25 14:57:23 +0200

    ADD: PyPI classifiers and keywords

--- setup.py
+++ setup.py
@@ -13,6 +13,31 @@ setup(
     author_email='wojciech.wentland@int.pl',
     description='Abstraction layer for Python Qt networks.',
     long_description=doc,
-    python_requires='>=3.5',
-    zip_safe=False
+    long_description_content_type='text/x-rst',
+    python_requires='>=3.6',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+
+        'Programming Language :: Python :: Implementation :: CPython',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+    ],
+    keywords=['server',
+              'client',
+              'pyqt5',
+              'pyside2',
+              'qtpy',
+              'tcp'],
 )
